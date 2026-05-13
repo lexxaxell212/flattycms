d8815d2191d19877fb5d727973166d3c227540e2
lexxaxell212 <febryagung404@gmail.com>
2026-05-13 21:55:21 +0700

    update

--- public/admin/index.php
+++ public/admin/index.php
@@ -5,8 +5,9 @@ autoload_core();
 require_once ROOT_PATH . "maintenance.php";
 
 // Auth guard admin
-if (!is_logged_in() || !is_admin()) {
-    redirect('/login');
+if (empty($_SESSION['admin_id'])) {
+    header('Location: /login.php');
+    exit;
 }
 
 require_once ADMIN_PATH . "router.php";

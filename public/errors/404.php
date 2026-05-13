ef9bb13f717bc16e0fb57f32c9c0e16aa8845dac
lexxaxell212 <febryagung404@gmail.com>
2026-05-13 22:21:11 +0700

    update

--- public/errors/404.php
+++ public/errors/404.php
@@ -1,10 +1,6 @@
 <?php
-require_once dirname(__DIR__, 2) . "/bootstrap.php";
-autoload_core();
-
 http_response_code(404);
 $page_title = "404";
-require_once SRC_PATH . "header.php";
 ?>
 
  <div class="py-5 text-center">
@@ -12,5 +8,3 @@ require_once SRC_PATH . "header.php";
    <p>Halaman yang kamu cari tidak ditemukan.<br>Mungkin sudah dipindah atau dihapus.</p>
  </div>
  
-<?php
-require_once SRC_PATH . "footer.php"; ?>

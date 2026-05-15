c6a0198b99c18121f262f199c08673930ed74852
lexxaxell212 <febryagung404@gmail.com>
2026-05-15 15:43:11 +0700

    update

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -4,7 +4,3 @@ safe_include(SRC_PATH . "partials/part-kenapa.php", "Kenapa Bandung");
 safe_include(SRC_PATH . "partials/part-blogs.php", "Artikel Terbaru");
 safe_include(SRC_PATH . "partials/part-informasi.php", "Update Terkini");
 ?>
-
-<a href="/api/auth/google.php" class="btn btn-outline-danger">
-    <i class="fab fa-google me-2"></i>Login with Google
-</a>

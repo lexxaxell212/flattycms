c4855823d6d76f685855e215b728829b47ad447f
lexxaxell212 <febryagung404@gmail.com>
2026-05-19 12:33:47 +0700

    update-v1.0-files

--- src/views/maintenance-page.php
+++ src/views/maintenance-page.php
@@ -2,7 +2,7 @@
 <html lang="id">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sedang Maintenance — <?= SITE_NAME ?></title>
   <meta name="robots" content="noindex, nofollow">
   <link rel="stylesheet" href="<?= CSS_URL ?>etc.css">

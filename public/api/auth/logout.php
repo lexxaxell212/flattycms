acb9b0fba406eb4e37bbd8dc4ce47e051f16361a
lexxaxell212 <febryagung404@gmail.com>
2026-05-21 14:46:34 +0700

    update-v1.0-files

--- public/api/auth/logout.php
+++ public/api/auth/logout.php
@@ -1,10 +1,15 @@
 <?php
 require_once dirname(__DIR__, 3) . "/bootstrap.php";
 autoload_core();
+
 $redirectout = $_SESSION['redirect_after_logout'] ?? '/';
-unset($_SESSION['redirect_after_logout']);
+
+session_unset();
 session_destroy();
+
 session_start();
 $_SESSION['flash'] = ['type' => 'success', 'message' => 'Berhasil logout'];
+session_write_close();
+
 header('Location: ' . $redirectout);
 exit;

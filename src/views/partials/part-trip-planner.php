7fe2873e736d91cc71d54d8446cc18846e62938c
lexxaxell212 <febryagung404@gmail.com>
2026-05-22 09:08:26 +0700

    update-v1.0-files

--- src/views/partials/part-trip-planner.php
+++ src/views/partials/part-trip-planner.php
@@ -1,7 +1,12 @@
 <div class="container-fluid">
 <div class="container">
-  <section id="part-trip-planner">
+  <section id="trip-planner">
     <h2>RENCANAKAN PERJALANMU</h2>
+    <p>Buat rencana perjalanan spesial</p>
   </section>
+  <div class="bg-gray text-center" id="show-gsi">
+    <i class="fa-spin"></i>
+    <p>Segera hadir</p>
+  </div>
 </div>
 </div>

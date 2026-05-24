c82c8aab18ca22cc5453414481ea585691375874
lexxaxell212 <febryagung404@gmail.com>
2026-05-24 21:05:33 +0700

    update-v1.0-files

--- src/views/pages/index.php
+++ src/views/pages/index.php
@@ -174,7 +174,7 @@ function _tdo_excerpt($html, $limit = 150) {
     </div>
     <?php endif; ?>
 
-    <?php if (!$featured && empty($pages) && empty($kuliner_items) && empty($budaya_items)): ?>
+    <?php if (!$_tdo_next && empty($_tdo_pages) && empty($kuliner_items) && empty($budaya_items)): ?>
     <div class="text-center py-5 text-muted">
       <i class="fas fa-file fa-3x mb-3 opacity-50 d-block"></i>
       Belum ada konten tersedia.

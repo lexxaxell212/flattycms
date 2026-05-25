14e2830a20ea2447d76ea060f9cd1262b78a40b5
lexxaxell212 <febryagung404@gmail.com>
2026-05-25 15:43:15 +0700

    update-v1.0-files

--- public/index.php
+++ public/index.php
@@ -1,32 +1,28 @@
 <?php
-////===///// LOAD CORE /////===////
 require_once __DIR__ . "/../bootstrap.php";
-////========/////////========////
 autoload_core();
 load_site_settings();
-/////////
-require_once LIB_PATH . 'analytics.php';
-record_pageview($pdo);
-///
-// Maintenance check
-require_once ROOT_PATH . "maintenance.php";
-// Router
+
 require_once PUBLIC_PATH . "router.php";
-// Session expired check untuk protected routes
+
 $protected_routes = ['user/profile'];
 if (in_array($uri, $protected_routes) && !isset($_SESSION['user'])) {
     header('Location: /');
     exit;
 }
-////// =============================== //////
+
+require_once ROOT_PATH . "maintenance.php";
+
 $page_title = $page_title ?? SITE_NAME;
-if (isset($view_content) && file_exists ($view_content)) {
+
+if (isset($view_content) && file_exists($view_content)) {
     require_once SRC_PATH . "headerv2.php";
+    require_once LIB_PATH . 'analytics.php';
+    record_pageview($pdo);
     require_once $view_content;
     require_once SRC_PATH . "footer.php";
 } else {
     http_response_code(404);
     require_once PUBLIC_PATH . "errors/404.php";
 }
-////// =============================== //////
 ?>

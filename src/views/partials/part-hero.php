07a44ad7b27add62ece872eb8887ac5a17f1d787
lexxaxell212 <febryagung404@gmail.com>
2026-05-29 03:23:36 +0700

    update-v1.0-files

--- src/views/partials/part-hero.php
+++ src/views/partials/part-hero.php
@@ -26,12 +26,12 @@ $slides = [
     <div class="glass-hero-card col-12 col-md-7 col-lg-5">
       <h1 class="mb-3" style="color:var(--dotid);">Jelajahi Bandung</h1>
       <p class="lead">Eksplorasi destinasi, kuliner, dan penginapan terbaik di Kota Kembang.</p>
-      <div class="d-flex flex-column gap-2">
-        <a href="/things-to-do" class="btn btn-outline-primary btn-sm">
-          Lihat Event
-        </a>
-        <a href="/trip" class="btn btn-primary btn-sm">
+      <div class="d-flex flex-column align-items-start gap-2">
+        <a href="/trip" class="btn btn-primary">
           Mulai Rencanakan <i class="arrow-icon fas fa-arrow-right ms-2"></i>
+        </a>
+        <a href="/things-to-do" class="btn btn-outline-primary">
+          Lihat Event
         </a>
       </div>
     </div>

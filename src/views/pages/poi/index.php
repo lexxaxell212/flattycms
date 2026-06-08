b1fed1c6051f9fc1de4aeac01c13ec7a93ab5dae
lexxaxell212 <febryagung404@gmail.com>
2026-06-08 13:17:59 +0700

    update-v1.1-files

--- src/views/pages/poi/index.php
+++ src/views/pages/poi/index.php
@@ -1,8 +1,8 @@
 <?php
+var_dump($slug);
 require_once LIB_PATH . 'poi-actions.php';
 $poi = get_poi_by_slug($slug);
-
-$page_title = $poi['name'] . ' - ' . SITE_NAME;
+var_dump($poi);
 ?>
 <img src="<?= $poi['poi_image'] ?>" alt="<?= htmlspecialchars($poi['name']) ?>" class="img-fluid w-100 mb-4">
       <h1><?= htmlspecialchars($poi['name']) ?></h1>

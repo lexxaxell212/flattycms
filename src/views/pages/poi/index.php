9ac7c97aff7d66eeba82aa399e5e727628031a42
lexxaxell212 <febryagung404@gmail.com>
2026-06-08 12:47:54 +0700

    update-v1.1-files

--- src/views/pages/poi/index.php
+++ src/views/pages/poi/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once LIB_PATH . 'poi-actions.php';
 //require_once LIB_PATH . 'v-reactions-page.php';
-var_dump($slug)
+var_dump($slug);
 $poi = get_poi_by_slug($slug);
 
 $page_title = $poi['name'] . ' - ' . SITE_NAME;

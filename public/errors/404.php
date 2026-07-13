62096583c30aed8cf452c8037e0b1a10ba92aed1
lexxaxell212 <febryagung404@gmail.com>
2026-07-13 08:46:04 +0700

    update-v1.2.2

--- public/errors/404.php
+++ public/errors/404.php
@@ -42,7 +42,8 @@ require_once SRC_PATH . "headerv2.php";
      position: absolute;
      inset: 0;
      width: 100%;
-     height: 100%;
+     height: 100dvh;
+     max-height: 1920px;
      object-fit: cover;
      filter: saturate(0.9) brightness(0.85);
     }
